09baf3a459c77dfe1b70796a55f5686a0b9e9a53
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-23 10:02:14 +0300

    Add files via upload

--- BankeyApp/BankeyApp/LoginViewController.swift
+++ BankeyApp/BankeyApp/LoginViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let loginView = LoginView()
-
+    
+    let signInButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -23,16 +25,41 @@ extension LoginViewController{
     private func style() {
         //first of all, turn off this, at start
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8 // for indicator spacing
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton) // everything u made, u must add it
         
+        //LoginView
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
+        
+        //SignButtom
+        NSLayoutConstraint.activate([
+//    view.trailingAnchor.constraint(equalToSystemSpacingAfter: signInButton.trailingAnchor, multiplier: 1) ya da
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2)
+
+        ])
+    }
+    
+    
+}
+extension LoginViewController {
+    
+    @objc func signInTapped(){
+        
     }
 }
 
